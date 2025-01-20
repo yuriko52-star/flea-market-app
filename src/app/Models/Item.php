16324614cc52af9eccd720c92cb9712bcb477d6c
yuriko <yuriko40223@gmail.com>
2025-01-20 16:37:23 +0900

    テーブル作成1

---
+++ src/app/Models/Item.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'seller_id','name','brand_name','image','price','description','condition_id'
+    ];
+}
